add776ec9807371b2f531f714dd812926cf7a350
alamin <alamin20192019@gmail.com>
2026-05-04 19:35:56 +0600

    chnaeg added

--- src/app/pages/view-all-profiles/view-all-profiles.component.ts
+++ src/app/pages/view-all-profiles/view-all-profiles.component.ts
@@ -1,29 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { OperatorProfileStore } from '../../services/operator-profile-store.service';
 
 @Component({
   selector: 'app-view-all-profiles',
   standalone: true,
-  imports: [MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [MatCardModule, MatButtonModule, RouterLink],
   templateUrl: './view-all-profiles.component.html',
   styleUrl: './view-all-profiles.component.scss'
 })
 export class ViewAllProfilesComponent {
-  pictureFileName = '';
-  cvFileName = '';
-
-  onPictureSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    const file = input.files?.[0];
-    this.pictureFileName = file?.name ?? '';
-  }
-
-  onCvSelected(event: Event): void {
-    const input = event.target as HTMLInputElement;
-    const file = input.files?.[0];
-    this.cvFileName = file?.name ?? '';
-  }
+  readonly store = inject(OperatorProfileStore);
 }
